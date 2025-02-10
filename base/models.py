a4b2e19b1659d2a3453d5f3c45dd0a59da507c39
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-02-10 22:48:22 +0500

    feat: add created_at models

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ class Blog(models.Model):
     body = models.TextField()
     type = models.CharField(max_length=255)
     image = models.FileField(upload_to='media/images/') 
-    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
