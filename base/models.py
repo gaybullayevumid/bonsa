cb8618b092c5d15fffde7d4dda25b28e81967e54
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-02-15 00:02:27 +0500

    feat: add category model

--- base/models.py
+++ base/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
-    type = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='blogs', null=True, blank=True)
     image = models.FileField(upload_to='media/images/') 
     created_at = models.DateTimeField(auto_now_add=True)
 
