279cb63232adfe1e4882378e9082149bb0f528ff
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-04-07 23:15:05 +0500

    feat: registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,21 +1,12 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-
-        if password and confirm_password and password != confirm_password:
-            raise forms.ValidationError("Parollar mos kelmadi!")
-
-        return cleaned_data
+        fields = ['username', 'email', 'password', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import user_logged_in
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -7,6 +10,19 @@ class SignInPageView(TemplateView):
 
 class SignUpPageView(TemplateView):
     template_name = 'account/sign-up.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserCreationForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, self.template_name, {'form': form})
 
 class RecoverPasswordPageView(TemplateView):
     template_name = 'account/recover-password.html'
